2f86358e9c8db8b97178dfcaf237c16b516c7593
ferdinand-arkis <ferdinand@arkis.cloud>
2022-09-18 20:42:48 +0800

    refactor create fibonacci logic, add some comments

--- web/web/fib/tasks.py
+++ web/web/fib/tasks.py
@@ -18,32 +18,40 @@ def create_fibonacci(n):
     # 0,1,1,2,3,5,8,13,etc
     n = int(n)
     fibs = FibonacciHistory.objects.all()
-    index = 0
 
-    max_num = fibs.aggregate(Max('index'))
-    current_max = max_num['index__max'] or 0
-    
+    existing_max_index = fibs.aggregate(Max('index'))
+    current_max_index = existing_max_index['index__max'] or 0
+    fib_count = fibs.count()
+
     val1 = 0
     val2 = 1
-    if current_max <= 0:
-        pass
-    else:
-        second_max = current_max - 1
-        val1 = int(fibs.get(index=current_max).fibonacci_number)
-        val2 = int(fibs.get(index=second_max).fibonacci_number)
-        index = current_max + 1
 
+    if current_max_index > 1:
+        # Only start getting values from DB if there are 2 or more indexes stored
+        second_to_max_index = current_max_index - 1
+        val1 = int(fibs.get(index=current_max_index).fibonacci_number)
+        val2 = int(fibs.get(index=second_to_max_index).fibonacci_number)
+
+    index = current_max_index
     while index <= n:
-        # Edge case if there is no current fib sequence
-        if index == 0:
+        if index > 0 and index == current_max_index:
+            index += 1
+            continue
+
+        if index == 0 and fib_count <= 0:
+            # Case 1: When there is no existing data in db yet
+            # Assume at 0 index which has 0 value
             fib = 0
-            val1 = 0
-            val2 = 1
+            index = 0
+        elif index == 0 and fib_count == 1:
+            # Case 2: When there is existing data but only at index 0
+            # Assume that we are at index 1 with value 1
+            fib = 1
+            index = 1
         else:
             fib = val1 + val2
-            tmp = copy(val1)
+            val2 = copy(val1)
             val1 = fib
-            val2 = tmp
 
         FibonacciHistory.objects.create(
             index=index,
