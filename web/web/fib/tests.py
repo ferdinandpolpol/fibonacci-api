f12a13e5100e37f2e76764e0b8af640b67aad38f
ferdinand-arkis <ferdinand@arkis.cloud>
2022-09-18 12:48:17 +0800

    simple test cases

--- web/web/fib/tests.py
+++ web/web/fib/tests.py
@@ -1,24 +1,52 @@
 
 import time 
 
-from django.test import TransactionTestCase, Client
+from django.test import TransactionTestCase, Client, override_settings
 from web.fib import tasks
 
-class FooTaskTestCase(TransactionTestCase):
+
+@override_settings(
+    task_eager_propagates=True,
+    task_always_eager=True,
+    broker_url='memory://',
+    backend='memory'
+)
+class FibonacciAPIViewTestCase(TransactionTestCase):
+    url = '/api/fibonacci/'
+
+    def setUp(self):
+        super().setUp()
+
+        self.client = Client()
+
+    def test__fib_api_post(self):
+        """ Test that the POST API endpoint is working """
+        res = self.client.post(self.url, {'n': 1})
+
+        self.assertEqual(res.status_code, 200)
 
     def test__fib_api(self):
-        client = Client()
-        res = client.post('/api/fibonacci/', {'n': 1})
+        """ Test that pending data will work """
+        res = self.client.post(self.url, {'n': 10})
         data = res.json()
 
         self.assertEqual("pending", data["status"])
-        
-        time.sleep(5)
 
         # Re-do the API call and assert that there is now data that is being returned
-        res = client.post('/api/fibonacci/', {'n': 1})
+        res = self.client.post(self.url, {'n': 10})
         data = res.json()
-        print(data)
         
         self.assertTrue(data["nth"] is not None)
         self.assertTrue("success", data["status"])
+
+    def test__fib_api(self):
+        """ Assert that the first 10 sequence of the fib is correct """
+        res = self.client.post(self.url, {'n': 10})
+
+        # Assert known sequence
+        known_sequence = [0,1,1,2,3,5,8,13,21,34]
+        for i in range(0, 10):
+            res = self.client.post(self.url, {'n': i})
+            data = res.json()
+            
+            self.assertEqual(known_sequence[i], int(data["nth"]))
